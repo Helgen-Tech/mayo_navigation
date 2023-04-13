901de31afa9b9a6d5888894a564f174300f98bde
Tom Dorian <tom@helgen.tech>
2023-04-13 12:03:51 +0100

    Adding git Ignore to package building files
    Signed-off-by: Tom Dorian <tom@helgen.tech>

--- build/mayo_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tom/ros2_workspace/src/navigation/mayo_navigation
-# Build directory: /home/tom/ros2_workspace/src/navigation/mayo_navigation/build/mayo_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
